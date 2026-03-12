4f62037d186055f4876de02b7b6d476ad8e8a837
Foo Jayden <deicidedace@gmail.com>
2026-03-12 17:54:33 +0800

    initial Spring Boot project commit

---
+++ src/main/java/com/example/facilitybooking/FacilityBookingApplication.java
@@ -0,0 +1,11 @@
+package com.example.facilitybooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilityBookingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FacilityBookingApplication.class, args);
+    }
+}
